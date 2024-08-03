ab42333b39c40139e6ad99333c7122d6f7153429
sxclij <sxclij@gmail.com>
2024-08-03 14:02:32 +0000

    .

--- main.c
+++ main.c
@@ -1,6 +1,44 @@
 // stdioのみmalloc不使用オーガニック縛り
 #include <stdio.h>
 
+int med3(int x, int y, int z) {
+    if (x < y)
+        if (y < z)
+            return y;
+        else if (z < x)
+            return x;
+        else
+            return z;
+    else if (z < y)
+        return y;
+    else if (x < z)
+        return x;
+    else
+        return z;
+}
+void quicksort(int a[], int left, int right) {
+    if (left < right) {
+        int i = left, j = right;
+        int tmp, pivot = med3(a[i], a[i + (j - i) / 2], a[j]);
+        while (1) {
+            while (a[i] < pivot)
+                i++;
+            while (pivot < a[j])
+                j--;
+            if (i >= j)
+                break;
+            tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+            i++;
+            j--;
+        }
+        quicksort(a, left, i - 1);
+        quicksort(a, j + 1, right);
+    }
+}
+
 int main() {
-    
+
+    return 0;
 }
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t ll;
+typedef pair<ll, ll> pi;
+
+#define rep(i, n) for(ll i=0;i<n;i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+}
