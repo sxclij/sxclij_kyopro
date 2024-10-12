094af44522af51266cf590bb0a43e82be5876675
sxclij <sxclij@gmail.com>
2024-10-12 12:54:52 +0000

    abc375-c

--- main.c
+++ main.c
@@ -1,65 +1,35 @@
 // stdioのみmalloc不使用オーガニック縛り
-#pragma GCC target("avx2")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-
-#include <math.h>  // この問題、縛り無理ゲーで草
 #include <stdio.h>
 
-int med3(int x, int y, int z) {
-    if (x < y)
-        if (y < z)
-            return y;
-        else if (z < x)
-            return x;
-        else
-            return z;
-    else if (z < y)
-        return y;
-    else if (x < z)
-        return x;
-    else
-        return z;
-}
-void quicksort(int a[], int left, int right) {
-    if (left < right) {
-        int i = left, j = right;
-        int tmp, pivot = med3(a[i], a[i + (j - i) / 2], a[j]);
-        while (1) {
-            while (a[i] < pivot)
-                i++;
-            while (pivot < a[j])
-                j--;
-            if (i >= j)
-                break;
-            tmp = a[i];
-            a[i] = a[j];
-            a[j] = tmp;
-            i++;
-            j--;
-        }
-        quicksort(a, left, i - 1);
-        quicksort(a, j + 1, right);
-    }
-}
+struct task {
+    int x;
+    int y;
+    char color;
+};
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    static double x[300000], y[300000];
+    static char a[3000][3000];
     for (int i = 0; i < n; i++) {
-        scanf("%lf %lf", &x[i + 1], &y[i + 1]);
+        scanf("%s", a[i]);
     }
 
-    double ans = 0;
-    for (int i = 0; i < n + 2; i++) {
-        double dx = x[i] - x[i + 1];
-        double dy = y[i] - y[i + 1];
-        double x = dx * dx + dy * dy;
-        ans += sqrt(x);
+    static struct task task[9000];
+    int task_size = 0;
+    for (int i = 1; i < n / 2 + 1; i++) {
+        for (int j = i; j < n + 1 - i + 1; j++) {
+            for (int k = i; k < n + 1 - i + 1; k++) {
+                task[task_size++] = (struct task){.x = k - 1, .y = n + 1 + j - 1, a[j - i][k - 1]};
+            }
+        }
+        for(int j=0;j<task_size;j++) {
+            a[task[j].x-1][task[j].y-1] = task[j].color;
+        }
+        task_size = 0;
     }
-    printf("%lf\n", ans);
-
-    return 0;
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", a[i]);
+    }
 }
