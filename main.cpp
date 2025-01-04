91a7e4bf84a5153aa2db6ef1c659f220eae102c8
sxclij <sxclij@gmail.com>
2025-01-04 15:10:45 +0000

    ?

--- main.cpp
+++ main.cpp
@@ -6,17 +6,6 @@ typedef pair<ll, ll> pi;
 
 #define rep(i, n) for (ll i = 0; i < n; i++)
 
-ll f1(ll src) {
-    string str = to_string(src);
-    ll sentou = str[0] - '0';
-    ll keta = str.size();
-    ll dst = 1;
-    for (ll i = 1; i <= keta; i++) {
-        dst *= sentou - (str[i] - '0');
-    }
-    return dst;
-}
-
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
@@ -25,24 +14,47 @@ int main() {
     cin >> h >> w;
 
     vector<pi> task;
+    ll start_muki;
 
     char t1;
     char s[1002][1002] = {'#'};
-    ll best[1002][1002] = {1000000};
+    ll cost[1002][1002] = {1000000};
     rep(i, h) rep(j, w) {
         cin >> t1;
         s[i + 1][j + 1] = t1;
         if (t1 == 'S') {
             task.push_back(make_pair(i + 1, j + 1));
+            start_muki = i + 1 + j + 1 % 2;
         }
     }
     while (task.size() != 0) {
         pi current_src = task.back();
         task.pop_back();
-        pi current_dst1 = make_pair(current_src.first + 1, current_src.second);
-        pi current_dst2 = make_pair(current_src.first - 1, current_src.second);
-        ll current_best1 = best[current_dst1.first][current_dst1.second];
-        ll current_score = best[current_src.first][current_src.second] + 1;
-        if ()
+        if (s[current_src.first][current_src.second] == 'G') {
+            cout << cost[current_src.first][current_src.second] << end;
+            ;
+            return 0;
+        }
+        ll current_score = cost[current_src.first][current_src.second] + 1;
+        pi current_dst1;
+        pi current_dst2;
+        if (current_src.first + current_src.second % 2 == start_muki) {
+            current_dst1 = make_pair(current_src.first, current_src.second + 1);
+            current_dst2 = make_pair(current_src.first, current_src.second - 1);
+        } else {
+            current_dst1 = make_pair(current_src.first + 1, current_src.second);
+            current_dst2 = make_pair(current_src.first - 1, current_src.second);
+        }
+        ll current_best1 = cost[current_dst1.first][current_dst1.second];
+        ll current_best2 = cost[current_dst2.first][current_dst2.second];
+        if (current_score > current_best1 && s[current_dst1.first][current_dst1.second] != '#') {
+            task.push_back(current_dst1);
+            cost[current_dst1.first][current_dst1.second] = current_score;
+        }
+        if (current_score > current_best2 && s[current_dst2.first][current_dst2.second] != '#') {
+            task.push_back(current_dst2);
+            cost[current_dst2.first][current_dst2.second] = current_score;
+        }
     }
+    cout << (ll)(-1) << endl;
 }
